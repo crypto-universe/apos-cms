8e8be5c0df939e5b78c6ef6e885397e54d9de8e4
Inpakeo <gilmanov91@gmail.com>
2024-06-28 17:37:47 +0200

    v.8.3
    1. Добавил возможность ставить h1 заголовки в тексте статьи
    2. Убрал внешний шрифт, добавил 4 стиль набора TODO со временем удалить другой
    3. Demo Welcome блок для страницы TODO вывести в виджет Typing
    4. Убрал меню все пункты на экранах поменьше
    5. Исправил портфолио + добавил навигацию на мобильном разрешении
    6. Убрал лишний код, который при загрузке на внутренней страницы скрипт
    7. Чистка slick со временем переписать на менее жирный фрейм

--- modules/article/index.js
+++ modules/article/index.js
@@ -54,12 +54,28 @@ module.exports = {
                   label: 'Paragraph (P)'
                 },
                 {
+                  tag: 'h1',
+                  label: 'Heading 1 (H1)'
+                },
+                {
+                  tag: 'h2',
+                  label: 'Heading 2 (H2)'
+                },
+                {
                   tag: 'h3',
                   label: 'Heading 3 (H3)'
                 },
                 {
                   tag: 'h4',
                   label: 'Heading 4 (H4)'
+                },
+                {
+                  tag: 'h5',
+                  label: 'Heading 5 (H5)'
+                },
+                {
+                  tag: 'h6',
+                  label: 'Heading 6 (H6)'
                 }
               ],
               insert: [
--- modules/portfolio-widget/ui/src/index.js
+++ modules/portfolio-widget/ui/src/index.js
@@ -1,7 +1,6 @@
 // import Swiper JS
 import Swiper from 'swiper/bundle';
-// import PhotoSwipeLightbox from 'photoswipe/lightbox';
-// import PhotoSwipe from 'photoswipe';
+
 
 export default () => {
   apos.util.widgetPlayers['image-gallery'] = {
@@ -9,14 +8,13 @@ export default () => {
     player: function (el) {
       const slides = el.dataset.slides || 1;
 
-      // Adjusting Swiper.js initialization for responsive breakpoints
       const swiper = new Swiper(el, {
-        slidesPerView: 1, // Default to 1 slide per view for mobile
-        spaceBetween: 10, // Reduced space for mobile
-        loop: true, // Enable loop mode
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
         autoplay: {
-          delay: 2500, // Delay in ms between auto transitions (e.g., 2500ms)
-          disableOnInteraction: true // Continue autoplay on swipe
+          delay: 2500,
+          disableOnInteraction: true
         },
         navigation: {
           nextEl: '.button-arrow--next',
@@ -30,54 +28,6 @@ export default () => {
         }
       });
 
-      // Commented out the PhotoSwipe lightbox and gallery initialization
-      /*
-      const photoSwipeOptions = {
-        mainClass: 'imageGallery--pswp',
-        gallery: '#imageGallery',
-        pswpModule: PhotoSwipe,
-        // set background opacity
-        bgOpacity: 1,
-        showHideOpacity: true,
-        children: 'a',
-        loop: true,
-        showHideAnimationType: 'fade',
-
-        // Click on image moves to the next slide
-        imageClickAction: 'next',
-        tapAction: 'next',
-
-        // Hiding a specific UI element
-        zoom: false,
-        close: true,
-        counter: true,
-        arrowKeys: true
-      };
-
-      const lightbox = new PhotoSwipeLightbox(photoSwipeOptions);
-
-      lightbox.init();
-
-      lightbox.on('change', () => {
-        const { pswp } = lightbox;
-        swiper.slideTo(pswp.currIndex, 0, false);
-      });
-
-      lightbox.on('afterInit', () => {
-        if (swiper.params.autoplay.enabled) {
-          swiper.autoplay.stop();
-        }
-      });
-
-      lightbox.on('closingAnimationStart', () => {
-        const { pswp } = lightbox;
-        swiper.slideTo(pswp.currIndex, 0, false);
-        // if autoplay enabled == true -> autoplay.start() when close lightbox
-        if (swiper.params.autoplay.enabled) {
-          swiper.autoplay.start();
-        }
-      });
-      */
     }
   };
 };
